5296a2c9ffa726cd343ae82fadf966bf5ea8c61e
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-01-29 17:50:53 +0900

    引数の修正

--- annoworkapi/exceptions.py
+++ annoworkapi/exceptions.py
@@ -2,3 +2,11 @@ class AnnoworkApiException(Exception):
     """
     annoworkapi に関するException
     """
+
+
+
+
+class CredentialsNotFoundError(AnnoworkApiException):
+    """
+    Annoworkの認証情報が見つからないときのエラー
+    """
